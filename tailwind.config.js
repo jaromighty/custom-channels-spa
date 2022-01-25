7748535044e3eae2341bfb61a34f598a5c2d7e58
Jarom Rial <jarom.rial@gmail.com>
2022-01-25 10:17:11 -0700

    Initial commit from gatsby: (https://github.com/jaromighty/gatsby-starter-hello-world-with-tailwindcss.git)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'media', // false or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
